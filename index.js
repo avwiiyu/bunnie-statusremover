75dc275ed0dc9d481a2d246d8b546a75b2ca6186
avwiiyu <avwiiyu@gmail.com>
2024-12-12 16:39:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["shop now!", "<3"];
+const statusMessages = ["shop now!", "create ticket to order"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
